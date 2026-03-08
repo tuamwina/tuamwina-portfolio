6cfbcd75ad8ff8e049d65e0688dbf125a4d41f35
tuamwina <tuamwinac@gmail.com>
2026-03-08 14:47:58 +0200

    Added dark mode toggle

--- script.js
+++ script.js
@@ -89,6 +89,30 @@ function typeEffect() {
 
 } 
 
- 
+
 
 typeEffect(); 
+
+const backToTopButton = document.getElementById('back-to-top'); 
+
+ window.addEventListener('scroll', function() {
+
+    if (window.scrollY > 100) { 
+
+        backToTopButton.style.display = 'block'; 
+
+    } else { 
+
+        backToTopButton.style.display = 'none'; 
+
+    } 
+
+}); 
+
+ 
+
+backToTopButton.addEventListener('click', function() { 
+
+    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+
+}); 
